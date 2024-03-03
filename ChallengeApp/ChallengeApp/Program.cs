267e8b79b84eb5fec98316bf83010def91e49555
Iwa1971 <iwus275@gmail.com>
2024-03-03 13:49:56 +0000

    zadanie domowe 4 dzien

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,10 +1,10 @@
 ﻿
 
 string name = "Ewa";
-bool kobieta = true;
+bool woman = true;
 int age = 30;
 
-if (kobieta && age < 30)
+if (woman && age < 30)
 {
     Console.WriteLine("Kobieta ponizej 30 lat");
 }
@@ -12,11 +12,9 @@ else if (name == "Ewa" && age == 30)
 {
     Console.WriteLine("Ewa lat 30");
 }
-else if (!kobieta && age < 30)
+else if (!woman && age < 18)
 {
-    Console.WriteLine("Niepelnoletni mezczyzna");  
+    
+  Console.WriteLine("Niepelnoletni mezczyzna");  
 }
-else
-{
-    Console.WriteLine();
-}
+
