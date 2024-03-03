3988993848bc3131207ef7366490bf6ce30ead5d
Iwa1971 <iwus275@gmail.com>
2024-03-03 12:26:13 +0000

    zadanie domowe dzien4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string name = "Ewa";
+bool kobieta = true;
+int age = 30;
+
+if (kobieta && age < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa lat 30");
+}
+else if (!kobieta && age < 30)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");  
+}
+else
+{
+    Console.WriteLine();
+}
