26cc40349bf9f9d824556f8854f7191408c137be
Iwa1971 <iwus275@gmail.com>
2024-03-11 21:54:45 +0000

    Z.D 5

---
+++ ChallengeApp/ChallengeApp/Employees.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+
+
+namespace ChallengeApp
+
+{
+
+    public class Employee
+
+    {
+        private List<int> score = new List<int>();
+        
+        public string name { get; private set; }
+        public string surname { get; private set; }
+        public int age { get; private set; }
+        public Employee(string name, string surname, int age)
+
+
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+        
+        }
+
+        public int result
+        {
+            get
+            {
+                return score.Sum();
+            }
+        }
+        public void AddGrade(int grade) => score.Add(grade);
+
+        }
+          
+    }
+
+
+
+
+
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,20 +1,51 @@
-﻿
+﻿using ChallengeApp;
+using System;
 
-string name = "Ewa";
-bool woman = true;
-int age = 30;
+Employee employee1 = new Employee("Anna", "Troska", 33);
+Employee employee2 = new Employee("John", "Russo", 44);
+Employee employee3 = new Employee("Iwona", "Mak", 29);
 
-if (woman && age < 30)
+List<Employee> employees = new List<Employee>()
 {
-    Console.WriteLine("Kobieta ponizej 30 lat");
-}
-else if (name == "Ewa" && age == 30)
-{
-    Console.WriteLine("Ewa lat 30");
-}
-else if (!woman && age < 18)
-{
-    
-  Console.WriteLine("Niepelnoletni mezczyzna");  
-}
+    employee1, employee2, employee3
+};
+
+
+
+employee1.AddGrade(7);
+employee1.AddGrade(9);
+employee1.AddGrade(16);
+employee1.AddGrade(11);
+employee1.AddGrade(2);
+
+employee2.AddGrade(17);
+employee2.AddGrade(8);
+employee2.AddGrade(11);
+employee2.AddGrade(21);
+employee2.AddGrade(12);
+
+employee3.AddGrade(1);
+employee3.AddGrade(19);
+employee3.AddGrade(6);
+employee3.AddGrade(10);
+employee3.AddGrade(12);
+
+    var maxResult = 0;
+    Employee EmployeeWithMaxResult = null!;
+
+    foreach (var employee in employees)
+
+    { 
+        if (employee.result > maxResult)
+        {
+             maxResult = employee.result;
+             EmployeeWithMaxResult = employee;   
+        }
+    }
+
+Console.WriteLine("Employee with the best Result");
+Console.WriteLine("Name:" + EmployeeWithMaxResult.name);
+Console.WriteLine("Surname:" + EmployeeWithMaxResult.surname);
+Console.WriteLine("Age:" + EmployeeWithMaxResult.age);
+Console.WriteLine("Points:" + EmployeeWithMaxResult.result);
 
