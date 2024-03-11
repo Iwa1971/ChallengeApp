83442d558bace06412e4013c91b98db1a50cc057
Iwa1971 <iwus275@gmail.com>
2024-03-11 21:54:45 +0000

    Z.D 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,20 +1,70 @@
-﻿
+﻿// Program ktory policzy ile jakich cyfr wystepuje w liczbie 367790
 
-string name = "Ewa";
-bool woman = true;
-int age = 30;
+int number = 367790; 
+string numberAsString = number.ToString();
+char[]letters = numberAsString.ToCharArray();
 
-if (woman && age < 30)
-{
-    Console.WriteLine("Kobieta ponizej 30 lat");
-}
-else if (name == "Ewa" && age == 30)
-{
-    Console.WriteLine("Ewa lat 30");
-}
-else if (!woman && age < 18)
-{
-    
-  Console.WriteLine("Niepelnoletni mezczyzna");  
-}
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
 
+foreach (char letter in letters)
+{
+    if (letter == '0')
+    {
+        counter0++;
+    }
+    else if (letter == '1')
+    {
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
+    }
+}
+    Console.WriteLine("0 =>" + counter0);
+    Console.WriteLine("1 =>" + counter1);
+    Console.WriteLine("2 =>" + counter2);
+    Console.WriteLine("3 =>" + counter3);
+    Console.WriteLine("4 =>" + counter4);
+    Console.WriteLine("5 =>" + counter5);
+    Console.WriteLine("6 =>" + counter6);
+    Console.WriteLine("7 =>" + counter7);
+    Console.WriteLine("8 =>" + counter8);
+    Console.WriteLine("9 =>" + counter9);
