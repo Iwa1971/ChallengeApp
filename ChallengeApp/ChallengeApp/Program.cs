4128eb699509bad36dc5e39060c867a546696cc1
Iwa1971 <iwus275@gmail.com>
2024-05-14 18:09:05 +0100

    clean code

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -11,33 +11,11 @@ employee.AddGrade("Piotr");
 employee.AddGrade(-2.5);
 
 var statistics = employee.GetStatistics();
-var statistics1 = employee.GetStatisticsWithForEach();
-var statistics2 = employee.GetStatisticsWithFor();
-var statistics3 = employee.GetStatisticsWithDoWhile();
-var statistics4 = employee.GetStatisticsWithWhile();
 
 Console.WriteLine($"Average:{statistics.Average:N2}");
 Console.WriteLine($"Max: {statistics.Max}");
 Console.WriteLine($"Min: {statistics.Min}");
 
-Console.WriteLine("Statistic with metod Foreach");
-Console.WriteLine($"Average:{statistics1.Average:N2}");
-Console.WriteLine($"Max: {statistics1.Max}");
-Console.WriteLine($"Min: {statistics1.Min}");
 
-Console.WriteLine("Statistic with metod For");
-Console.WriteLine($"Average:{statistics2.Average:N2}");
-Console.WriteLine($"Max: {statistics2.Max}");
-Console.WriteLine($"Min: {statistics2.Min}");
-
-Console.WriteLine("Statistic with metod Do and While");
-Console.WriteLine($"Average:{statistics3.Average:N2}");
-Console.WriteLine($"Max:{statistics3.Max}");
-Console.WriteLine($"Min:{statistics3.Min}");
-
-Console.WriteLine("Statistic with metod While");
-Console.WriteLine($"Average:{statistics4.Average:N2}");
-Console.WriteLine($"Max:{statistics4.Max}");
-Console.WriteLine($"Min:{statistics4.Min}");
 
 
